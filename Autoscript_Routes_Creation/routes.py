ff9fb9a7f741416b9cca9570d750d7589f7a9e7a
Imran Thoufeeque <75982271+I7Z3R0@users.noreply.github.com>
2021-05-21 05:52:21 +0530

    Update routes.py

--- Autoscript_Routes_Creation/routes.py
+++ Autoscript_Routes_Creation/routes.py
@@ -37,7 +37,7 @@ if __name__ == '__main__':
     if len(sys.argv[1:]) < 3:
         print('''
 
-    Usage : script.py <absolute path to ip address file> <gateway ip address> <destination port number>
+    Usage : script.py <absolute path to ip address file> <gateway ip address> <destination interface port number>
 
     Example : script.py /home/dummy/Desktop/fortigate/file.txt 10.1.1.1 6
 
